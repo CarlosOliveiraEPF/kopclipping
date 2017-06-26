dc9391da51e79d8290d96c25c3db696045a4a43e
Carlos Oliveira <vinicius.study@gmail.com>
2017-06-26 20:39:47 -0300

    Ajustes com os keys...

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 var TwitterPackage = require('twitter');
 
 var secret = {
-  consumer_key: 'YOURS',
-  consumer_secret: 'YOURS',
-  access_token_key: 'YOURS',
-  access_token_secret: 'YOURS'
+  consumer_key: 'NbPWv4YokBOvqUb27uBtYcw5P',
+  consumer_secret: 'F4S2qw5qUj1zowh8J3VkMRRkBwokAEXSDRCqAcLwfPxQDZTf2T',
+  access_token_key: '713714429-ATnlrIrW9P5KljawarU1w76KI1ghRYVpALDlGJWh',
+  access_token_secret: 'ZLoOCSwjC3Onjqk5lYjqcE5Jabr6ccZEVuqXO4FLFPBj5'
 }
 
 var Twitter = new TwitterPackage(secret);
 
-Twitter.stream('statuses/filter', {track: '@kopzinksi'}, function(stream) {
+Twitter.stream('statuses/filter', {track: '@RedeGlobo'}, function(stream) {
   stream.on('data', function(tweet) {
     console.log(tweet.text);
   });
